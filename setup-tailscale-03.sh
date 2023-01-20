2e72dd94e728701556782b08e8ce2ea0e88acf73
ropsnap <122769692+ropsnap@users.noreply.github.com>
2023-01-19 22:48:59 -0300

    Update setup-tailscale-03.sh

--- setup-tailscale-03.sh
+++ setup-tailscale-03.sh
@@ -2,15 +2,15 @@
 
 echo;
 echo;
-echo ' --------------------- Step [3] ------------------------------'
+echo ' --------------------- Step [3] -------------------------------'
 echo ' → File path c '$(pwd)'/setup-tailscale-03.sh'
-echo ' -------------------------------------------------------------'
+echo ' --------------------------------------------------------------'
 echo;
 
 sleep 2
 
 # ------------------------------------------------------------------ 1
-echo ' [1] Downloading and installing Tailscale CLI ----------------'
+echo ' [1] Downloading and installing Tailscale CLI -----------------'
 echo;
 
 sleep 3
@@ -18,7 +18,7 @@ sleep 3
 curl -fsSL https://tailscale.com/install.sh | sh
 
 # ------------------------------------------------------------------ 2
-echo ' [2] Enabling IP forwarding ----------------------------------'
+echo ' [2] Enabling IP forwarding -----------------------------------'
 echo;
 
 sleep 3
@@ -28,7 +28,7 @@ echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale
 sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
 
 # ------------------------------------------------------------------ 3
-echo ' [3] Enabling and starting tailscaled as system service ------'
+echo ' [3] Enabling and starting tailscaled as system service -------'
 echo;
 
 sleep 1
@@ -37,14 +37,14 @@ service tailscale start
 rc-update add tailscale
 
 # ------------------------------------------------------------------ 3
-echo ' [4] Connecting as client (ssh and exit-node) ----------------'
+echo ' [4] Connecting as client (ssh and exit-node) -----------------'
 echo;
 
 sudo tailscale up --ssh --exit-node --accept-dns --accept-risk all --auth-key tskey-auth-kT6X4C4CNTRL-3N48qNbyKw1EnrHj6FhC12ktDdX2dpHtT
 
-echo ' ✓ Success ---------------------------------------------------'
+echo ' ✓ Success ----------------------------------------------------'
 echo
-echo ' ⌛ Rebooting withing 6 seconds... ---------------------------'
+echo ' ⌛ Rebooting withing 6 seconds... '
 echo;
 
 sleep 6
