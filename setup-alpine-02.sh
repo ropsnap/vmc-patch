90ca3c7505bbfca84528d630df4ff82e114c23fd
ropsnap <122769692+ropsnap@users.noreply.github.com>
2023-01-19 22:47:31 -0300

    Update setup-alpine-02.sh

--- setup-alpine-02.sh
+++ setup-alpine-02.sh
@@ -13,19 +13,32 @@ echo ' [1] Setting up Alpine (setup-alpine) ------------------------'
 echo;
 echo ' * Answer this order: '
 echo;
-echo '	→ hostname:  localhost'
-echo '	→ interface: eth0'
-echo '	→ repository: 1'
-echo '	→ localhost: valor'
-echo '	→ localhost: valor'
-echo '	→ password: gag999'
+echo '	→ Hostname:  localhost'
+echo '	→ Interface: eth0'
+echo '	→ DNS Domain: enter'
+echo '	→ DNS Servers: enter'
+echo '	→ Password: gag999'
+echo '	→ Timezone: UTC'
+echo '	→ Proxy: none'
+echo '	→ NTP client: chrony'
+echo '	→ Mirror: 1'
+echo '	→ Setup- a user: no'
+echo '	→ SSH Server: openssh'
+echo '	→ Allow root ssh login: yes'
+echo '	→ root ssh key/URL : none'
+echo '	→ Disk: none'
+echo '	→ Store configs: none'
+echo '	→ APK Cache directory: enter'
+
+
+
 echo;
 echo ' ⌛ Loading...'
 echo;
 
 sleep 3
 
-setup-alpine && echo ' ✓ Success! Please, reboot -----------------------------------'
+setup-alpine;
 
 echo;
 echo ' [2] Updating and Upgrading packages ------------------------'
