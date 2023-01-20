6be1d8137c5f1175f41de30b5d9927743dc72ab3
ropsnap <122769692+ropsnap@users.noreply.github.com>
2023-01-20 16:14:20 -0300

    Update disk.sh

--- disk.sh
+++ disk.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo;
-echo ' ------------------ Patch Script → disk.sh -------------------'
-echo ' → This script makes vmc instance persistent disk storage'
-echo ' -------------------------------------------------------------'
+echo
+echo ' ------------------ Patch Script → disk.sh -------------------';
+echo ' → This script makes vmc instance persistent disk storage';
+echo ' -------------------------------------------------------------';
 echo;
 
-sleep 1
+sleep 3
 
-echo ' [1] Setting persistent volume -------------------------------'
+echo ' [1] Setting persistent volume -------------------------------';
 echo;
 
 export ROOT_SIZE=4096 && setup-disk -m sys -s 0 /dev/sda && touch pertest;
@@ -16,14 +16,14 @@ export ROOT_SIZE=4096 && setup-disk -m sys -s 0 /dev/sda && touch pertest;
 sleep 1
 
 echo
-echo ' ✓ Success! --------------------------------------------------'
+echo ' ✓ Success! --------------------------------------------------';
 echo;
 
-echo ' --------------------- Attention -----------------------------'
-echo ' 🟡 After reboot, run the next patch → main.sh'
-echo ' -------------------------------------------------------------'
+echo ' --------------------- Attention -----------------------------';
+echo ' 🟡 After reboot, run the next patch → main.sh';
+echo ' -------------------------------------------------------------';
 echo;
-echo ' ⌛ Rebooting withing 9 seconds... '
+echo ' ⌛ Rebooting withing 9 seconds... ';
 echo;
 
 sleep 9
