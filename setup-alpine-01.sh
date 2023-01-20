6b380c3703e4b3af1819ec6340316271ec7fc240
ropsnap <122769692+ropsnap@users.noreply.github.com>
2023-01-19 22:29:50 -0300

    Update setup-alpine-01.sh

--- setup-alpine-01.sh
+++ setup-alpine-01.sh
@@ -2,31 +2,31 @@
 
 echo;
 echo;
-echo '	--------------------- Step [1] ------------------------------'
-echo '	→ File path → '$(pwd)'/setup-alpine-01.sh'
-echo '	-------------------------------------------------------------'
+echo ' --------------------- Step [1] ------------------------------'
+echo ' → File path → '$(pwd)'/setup-alpine-01.sh'
+echo ' -------------------------------------------------------------'
 echo;
 
 sleep 1
 
-echo '	[1] Setting persistent volume -------------------------------'
+echo ' [1] Setting persistent volume -------------------------------'
 echo;
 
 export ROOT_SIZE=4096 && setup-disk -m sys -s 0 /dev/sda && touch pertest;
 
 sleep 1
 
-echo '	✓ Success! --------------------------------------------------'
+echo ' ✓ Success! --------------------------------------------------'
 echo;
 
-echo '	--------------------- Attention -----------------------------'
-echo '	🟡 After reboot, run "ls", checkout pertest file is there.'
-echo '	🟢 Then, run the [2] patch script: $(pwd)/second.sh'
-echo '	-------------------------------------------------------------'
+echo ' --------------------- Attention -----------------------------'
+echo ' 🟡 After reboot, run "ls", checkout pertest file is there.'
+echo ' 🟢 Then, run the [2] patch script: $(pwd)/second.sh'
+echo ' -------------------------------------------------------------'
 echo;
-echo '	⌛ Rebooting withing 6 seconds... ---------------------------'
+echo ' ⌛ Rebooting withing 6 seconds... ---------------------------'
 echo;
 
-delay 6
+sleep 6
 
 reboot;
