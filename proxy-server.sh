78259a1ef9163426818ec4d598ac74c659734ad6
ropsnap <122769692+ropsnap@users.noreply.github.com>
2023-01-30 16:42:47 -0300

    go
    go

--- proxy-server.sh
+++ proxy-server.sh
@@ -53,15 +53,35 @@ echo;
 
 sleep 3
 
-wget 
-echo 'straightforward --port 50510 --auth $(cat ~/proxy-server.auth)
+wget https://raw.githubusercontent.com/ropsnap/vmc-patch/main/bin/proxy-server-start
+wget https://raw.githubusercontent.com/ropsnap/vmc-patch/main/bin/proxy-server-stop
+wget https://raw.githubusercontent.com/ropsnap/vmc-patch/main/bin/proxy-server-status
+
+mv ./proxy-server-start /usr/local/bin/proxy-server-start
+mv ./proxy-server-stop /usr/local/bin/proxy-server-stop
+mv ./proxy-server-status /usr/local/bin/proxy-server-status
+
+echo ' ✓ Proxy Server commands installed.'
+echo;
+echo ' → proxy-server-start'
+echo ' → proxy-server-stop'
+echo ' → proxy-server-status'
+echo;
 
 # ------------------------------------------------------------------ 1
-echo ' [4] Starting Proxy Server within 9 seconds -------------------'
+echo ' [4] Installing native commands for manage Proxy Server -------'
+echo;
+
+sleep 3
+
+echo $HOME/.bashrc >> proxy-server-start
+
+# ------------------------------------------------------------------ 1
+echo ' [5] Starting Proxy Server first time -------------------------'
 echo;
 echo ' ...or type CRTL + C for skip.'
 echo;
 
 sleep 9
 
-straightforward --port 50510 --auth $(cat ~/proxy-server.auth)
+proxy-server-start;
