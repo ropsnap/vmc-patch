128b45239abb52b7f0e3bd0122f7e0872cf9d73d
ropsnap <122769692+ropsnap@users.noreply.github.com>
2023-01-31 02:17:02 -0300

    Update proxy-server.sh

--- proxy-server.sh
+++ proxy-server.sh
@@ -12,90 +12,29 @@ sleep 3
 
 # ------------------------------------------------------------------ 1
 
-echo ' [1] Checking ~/proxy-server.auth file ------------------------'
+echo ' [1] Fetching ~/tinyproxy.conf file ---------------------------'
 echo;
 
 sleep 3
 
-AUTH_FILE=~/proxy-server.auth;
-
-if [  ! -f "$AUTH_FILE" ]; then
-
-    echo ' ...creating Proxy Server auth file ~/proxy-server.auth';
-    echo;
-
-    PUSER=$RANDOM | md5sum | head -c 4;
-    PPASS=$RANDOM | md5sum | head -c 8;
-
-    # crates and wirtes proxy CLI auth arg
-    touch ~/proxy-server.auth
-    echo '"'$($PUSER):$($PPASS)'"' >> ~/proxy-server.auth
-
-    echo;
-    echo;
-fi
+wget https:// tinyproxy.conf
 
 # ------------------------------------------------------------------ 2
 
-echo ' [2] Installing essential packages and nodejs modules ---------'
+echo ' [2] Installing Tinyproxy -------------------------------------'
 echo;
 
 sleep 3
 
-# install 
-apk add nodejs 
-apk add npm
-
-# install node packages globally
-npm install -g straightforward 
-npm install -g nodemon
+apk add tinyproxy --force-broken-world;
 
 echo;
-echo ' ✓ nodejs, npm, straightforward & nodemon successfully installed.'
-echo;
-
-# ------------------------------------------------------------------ 3
-
-echo ' [3] Installing native commands for manage Proxy Server -------'
-echo;
-
-sleep 3
-
-wget https://raw.githubusercontent.com/ropsnap/vmc-patch/main/bin/proxy-server-start
-wget https://raw.githubusercontent.com/ropsnap/vmc-patch/main/bin/proxy-server-stop
-wget https://raw.githubusercontent.com/ropsnap/vmc-patch/main/bin/proxy-server-status
-
-mv ./proxy-server-start /usr/local/bin/proxy-server-start
-mv ./proxy-server-stop /usr/local/bin/proxy-server-stop
-mv ./proxy-server-status /usr/local/bin/proxy-server-status
-
-chmod 744 /usr/local/bin/proxy-server-start
-chmod 744 /usr/local/bin/proxy-server-stop
-chmod 744 /usr/local/bin/proxy-server-status
-
-echo;
-echo ' ✓ Proxy Server commands installed.'
-echo;
-
-echo ' → proxy-server-start'
-echo ' → proxy-server-stop'
-echo ' → proxy-server-status'
-echo;
-
-# ------------------------------------------------------------------ 4
-
-echo ' [4] Installing native commands for manage Proxy Server -------'
-echo;
-
-sleep 3
-
-echo sleep 3 >> $HOME/.bashrc
-echo proxy-server-start >> $HOME/.bashrc
+echo ' ✓ tinyproxy successfully installed.'
 echo;
 
 # ------------------------------------------------------------------ 5
 
-echo ' [5] Starting Proxy Server first time -------------------------'
+echo ' [3] Starting Proxy Server first time -------------------------'
 echo;
 echo ' ...or type CRTL + C for skip.'
 echo;
